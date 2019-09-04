3c7bff5562ecb4dda15088ac40e3a52cf3b7791f
MrNivorous <karn.sood@gmail.com>
2019-09-04 12:52:13 +0700

    080202

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {res.send('Welcome to AirBNB API')})
+
 app.listen(4000, () => {
 	console.log('Ready on port 4000')
 })
