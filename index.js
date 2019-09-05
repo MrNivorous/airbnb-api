c628e66b026f3cfd0c9dce8b10d734583c54d32c
MrNivorous <karn.sood@gmail.com>
2019-09-05 12:01:29 +0700

    090102

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+app.get(require('./database.js'))
 
 app.get('/', require('./controllers/root.js'))
 
