532e6db1994159633663c7ff29884af4919dfcf3
MrNivorous <karn.sood@gmail.com>
2019-09-05 14:14:29 +0700

    090301

---
+++ controllers/postPlaces.js
@@ -0,0 +1,17 @@
+const Place = require('../models/place.js')
+
+postPlaces = (req, res) => { Place.create({
+	title: 'Unbelievable Infinite Pool',
+	description: 'So nice!',
+	type: 'Entire House',
+	city: 'Ko Samui',
+	country: 'Thailand',
+	price: 350,
+	rating: 4,
+	guests: 5,
+	bathrooms: 5
+}).then(data => res.send(data))
+	.catch(err => {console.log(err)})
+}
+
+module.exports = postPlaces
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get(require('./database.js'))
 app.get('/', require('./controllers/root.js'))
 
 app.get('/places', require('./controllers/getPlaces.js'))
+app.post('/places', require('./controllers/postPlaces.js'))
 
 app.listen(4000, () => {
 	console.log('Ready on port 4000')
