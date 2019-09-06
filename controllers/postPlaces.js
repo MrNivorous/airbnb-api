9b983a8002851741591357caed07dece18d3e402
MrNivorous <karn.sood@gmail.com>
2019-09-06 11:44:40 +0700

    090502

--- controllers/postPlaces.js
+++ controllers/postPlaces.js
@@ -1,16 +1,7 @@
 const Place = require('../models/place.js')
 
-let postPlaces = (req, res) => { Place.create({
-	title: 'Unbelievable Infinite Pool',
-	description: 'Feels like I\'m Harry Potter',
-	type: 'A Closet',
-	city: 'Houston',
-	country: 'USA',
-	price: 50,
-	rating: 5,
-	guests: 0,
-	bathrooms: 0
-}).then(data => res.send(data))
+let postPlaces = (req, res) => {
+	Place.create(req.body).then(data => res.send(data))
 	.catch(err => {console.log(err)})
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const database = require('./database')
+const bodyParser = require('body-parser')
 
-
+// Express
 const app = express()
 
-app.get(require('./database.js'))
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+// Database
+database
+
+// Routes
 app.get('/', require('./controllers/root.js'))
-
 app.get('/places', require('./controllers/getPlaces.js'))
 app.post('/places', require('./controllers/postPlaces.js'))
 
