ea9b6ba99da532ff45ea1a9e112d1d79e6f2dfc2
MrNivorous <karn.sood@gmail.com>
2019-09-06 16:15:18 +0700

    090902 fixed - still need to make if statement

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -1,13 +1,5 @@
 const Place = require('../models/place.js')
 
 module.exports = (req, res) => {
-	Place.find({
-		price: {$lt: req.query.max_price},
-		bathrooms: {$gt: req.query.min_rooms},
-		guests: {$gt: req.query.min_guests}
-		})
-		.then(data => {res.send(data)})
-		.catch(err => {
-		res.send(err)
-		})
-	}
+	Place.findById(req.params.id).then(data => {res.send(data)}).catch(err => {res.send(err)})
+}
--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -1,7 +1,13 @@
 const Place = require('../models/place.js')
 
 module.exports = (req, res) => {
-	Place.find({})
-	.then(data => {res.send(data)})
-
-}
+	Place.find({
+		price: {$lt: req.query.max_price},
+		bathrooms: {$gt: req.query.min_rooms},
+		guests: {$gt: req.query.min_guests}
+		})
+		.then(data => {res.send(data)})
+		.catch(err => {
+		res.send(err)
+		})
+	}
