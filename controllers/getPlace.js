f3a534c1b91ca71c26f52978bcbae3209ce81fde
MrNivorous <karn.sood@gmail.com>
2019-09-06 12:20:01 +0700

    090901

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -1,7 +1,7 @@
 const Place = require('../models/place.js')
 
 module.exports = (req, res) => {
-	Place.findById(req.params.id).then(data => {res.send(data)}).catch(err => {
+	Place.findById(req.params.id).select('-guests').then(data => {res.send(data)}).catch(err => {
 	res.send(err)
 	})
 }
