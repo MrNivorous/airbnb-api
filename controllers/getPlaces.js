d33f81c96d77df412bcdacbd236cd5113ea273bf
MrNivorous <karn.sood@gmail.com>
2019-09-06 17:26:30 +0700

    090902

--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -1,13 +1,26 @@
 const Place = require('../models/place.js')
 
 module.exports = (req, res) => {
-	Place.find({
-		price: {$lt: req.query.max_price},
-		bathrooms: {$gt: req.query.min_rooms},
-		guests: {$gt: req.query.min_guests}
-		})
+
+search = () => {
+	let searchObj = {}
+	req.query.max_price ? searchObj.price = {$lte: req.query.max_price} : null
+	req.query.min_rooms ? searchObj.bathrooms = {$gte: req.query.min_rooms} : null
+	req.query.min_guests ? searchObj.guests = {$gte: req.query.min_guests} : null
+	return searchObj
+}
+
+	Place.find(search())
 		.then(data => {res.send(data)})
 		.catch(err => {
 		res.send(err)
 		})
 	}
+
+ // ( { $or: [ { quantity: { $lt: 20 } }, { price: 10 } ] } )
+
+// ({$or:[
+// 	{price: {$lte: req.query.max_price}},
+// 	{bathrooms: {$gte: req.query.min_rooms}},
+// 	// guests: {$gte: req.query.min_guests}}
+// ]})
