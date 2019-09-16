d08115be63456d853b3c2d0713b5d1f04d2ab52b
MrNivorous <karn.sood@gmail.com>
2019-09-16 12:56:33 +0700

    changed uri

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-let database = mongoose.connect(process.env.DATABASE, {useNewUrlParser: true}, (err) => {
+let database = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, (err) => {
 	err ? console.log(err) : console.log('Connected to MongoDB')
 })
 
